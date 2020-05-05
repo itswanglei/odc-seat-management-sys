f2ea716782b6243b46ee5cf08097fcb11b8191e3
Wang Lei <lei.wang1@thoughtworks.com>
2020-05-05 15:21:40 +0800

    feat: display all seats in green region

--- src/components/GreenRegion.vue
+++ src/components/GreenRegion.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <div>
-      <seat-group @edit-seat-info="openEditDialog"></seat-group>
+    <div class="odc-region">
+      <div class="horizontal-region">
+        <seat-group
+          v-for="(seatGroup, index) in 6"
+          :key="index"
+          @edit-seat-info="openEditDialog"
+          orientation="horizontal"
+        ></seat-group>
+      </div>
+      <div class="vertical-region">
+        <seat-group v-for="(seatGroup, index) in 10" :key="index" @edit-seat-info="openEditDialog"></seat-group>
+      </div>
+      <div class="vertical-region">
+        <seat-group v-for="(seatGroup, index) in 9" :key="index" @edit-seat-info="openEditDialog"></seat-group>
+      </div>
     </div>
     <seat-info-form ref="dialog" v-bind="currentSeatInfo" @submit="saveSeatInfo"></seat-info-form>
   </div>
@@ -44,3 +57,26 @@ export default {
   }
 };
 </script>
+
+<style>
+.odc-region {
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.horizontal-region {
+  width: 20%;
+  margin: 20px 20px 20px 200px;
+  display: flex;
+  flex-wrap: wrap-reverse;
+  justify-content: space-between;
+}
+
+.vertical-region {
+  width: 70%;
+  margin: 20px;
+  display: flex;
+}
+</style>
