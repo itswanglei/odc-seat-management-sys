d83d439a78a2b327c21dd3ef258544ef7abff429
Wang Lei <lei.wang1@thoughtworks.com>
2020-05-05 14:42:08 +0800

    feat: click seat to open edit dialog

--- src/components/GreenRegion.vue
+++ src/components/GreenRegion.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>Green Region</div>
+  <div>
+    <div>
+      <seat-group @edit-seat-info="openEditDialog"></seat-group>
+    </div>
+    <seat-info-form ref="dialog" v-bind="currentSeatInfo" @submit="saveSeatInfo"></seat-info-form>
+  </div>
 </template>
+
 <script>
-export default {};
+import { Message } from "element-ui";
+import SeatGroup from "./SeatGroup";
+import SeatInfoForm from "./SeatInfoForm";
+
+export default {
+  components: {
+    "seat-group": SeatGroup,
+    "seat-info-form": SeatInfoForm
+  },
+  data() {
+    return {
+      currentSeatInfo: {
+        seatId: "",
+        monitor1: "",
+        monitor2: "",
+        monitor3: "",
+        macmini: "",
+        tc: "",
+        user: "",
+        phone: ""
+      }
+    };
+  },
+  methods: {
+    openEditDialog(seatInfo) {
+      this.currentSeatInfo = seatInfo;
+      this.$refs["dialog"].handleOpen();
+      console.log(this.currentSeatInfo);
+    },
+    saveSeatInfo(seatInfo) {
+      Message.success("保存成功");
+      console.log(seatInfo);
+    }
+  }
+};
 </script>
---
+++ src/components/SeatInfoForm.vue
@@ -0,0 +1,152 @@
+<template>
+  <el-dialog
+    :visible.sync="visible"
+    title="编辑座位信息"
+    width="25%"
+    :before-close="resetFields"
+    @open="handleOpen"
+  >
+    <el-form :model="form" :rules="rules" ref="form" label-position="left" label-width="80px">
+      <el-form-item label="座位编号">
+        <el-input v-model="form.seatId" autocomplete="off" disabled></el-input>
+      </el-form-item>
+      <el-form-item label="显示器 #1" prop="monitor1">
+        <el-input v-model="form.monitor1" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="显示器 #2" prop="monitor2">
+        <el-input v-model="form.monitor2" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="显示器 #3" prop="monitor3">
+        <el-input v-model="form.monitor3" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Mac mini" prop="macmini">
+        <el-input v-model="form.macmini" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="TC 盒子" prop="tc">
+        <el-input v-model="form.tc" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="使用人">
+        <el-input v-model="form.user" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="联系电话" prop="phone">
+        <el-input v-model="form.phone" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="handleClose('form')">取 消</el-button>
+      <el-button type="primary" @click="handleSubmit('form')">保 存</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: [
+    "seatId",
+    "monitor1",
+    "monitor2",
+    "monitor3",
+    "macmini",
+    "tc",
+    "user",
+    "phone"
+  ],
+  data() {
+    return {
+      visible: false,
+      form: {
+        seatId: "",
+        monitor1: "",
+        monitor2: "",
+        monitor3: "",
+        macmini: "",
+        tc: "",
+        user: "",
+        phone: ""
+      },
+      rules: {
+        monitor1: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "设备编号应为8位数字",
+            trigger: "blur"
+          }
+        ],
+        monitor2: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "设备编号应为8位数字",
+            trigger: "blur"
+          }
+        ],
+        monitor3: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "设备编号应为8位数字",
+            trigger: "blur"
+          }
+        ],
+        macmini: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "设备编号应为8位数字",
+            trigger: "blur"
+          }
+        ],
+        tc: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "设备编号应为8位数字",
+            trigger: "blur"
+          }
+        ],
+        phone: [
+          {
+            type: "string",
+            pattern: /^\d{8}$/,
+            message: "联系电话应为11位数字",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleOpen() {
+      this.visible = true;
+      this.$nextTick(() => {
+        this.form.seatId = this.seatId;
+        this.form.monitor1 = this.monitor1;
+        this.form.monitor2 = this.monitor2;
+        this.form.monitor3 = this.monitor3;
+        this.form.macmini = this.macmini;
+        this.form.tc = this.tc;
+        this.form.user = this.user;
+        this.form.phone = this.phone;
+      });
+    },
+    handleClose(formName) {
+      this.$refs[formName].resetFields();
+      this.visible = false;
+    },
+    handleSubmit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$emit("submit", this.form);
+          this.$refs[formName].resetFields();
+          this.visible = false;
+        }
+      });
+    },
+    resetFields(done) {
+      this.$refs["form"].resetFields();
+      done();
+    }
+  }
+};
+</script>
