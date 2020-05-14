e614cf0ed09aa7ee796766c4e26dd30376b3a593
Wang Lei <lei.wang1@thoughtworks.com>
2020-05-14 22:15:48 +0800

    feat: display blue region seats

--- src/components/BlueRegion.vue
+++ src/components/BlueRegion.vue
@@ -1,6 +1,92 @@
 <template>
-  <div>Blue Region</div>
+  <div class="odc-region">
+    <seat-group
+      :seats="seatsData.tableA"
+      @edit-seat-info="openEditDialog"
+      orientation="horizontal"
+      firstSeatPosition="southeast"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableB"
+      @edit-seat-info="openEditDialog"
+      orientation="horizontal"
+      firstSeatPosition="southeast"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableC"
+      @edit-seat-info="openEditDialog"
+      orientation="horizontal"
+      order="counterclockwise"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableD"
+      @edit-seat-info="openEditDialog"
+      orientation="horizontal"
+      firstSeatPosition="southeast"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableE"
+      @edit-seat-info="openEditDialog"
+      order="counterclockwise"
+      firstSeatPosition="northwest"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableF"
+      @edit-seat-info="openEditDialog"
+      order="counterclockwise"
+      firstSeatPosition="northwest"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableG"
+      @edit-seat-info="openEditDialog"
+      order="counterclockwise"
+      firstSeatPosition="northwest"
+    ></seat-group>
+    <seat-group
+      :seats="seatsData.tableH"
+      @edit-seat-info="openEditDialog"
+      order="counterclockwise"
+      firstSeatPosition="northwest"
+    ></seat-group>
+    <seat-info-form ref="dialog"></seat-info-form>
+  </div>
 </template>
 <script>
-export default {};
+import SeatGroup from "./SeatGroup";
+import SeatInfoForm from "./SeatInfoForm";
+
+export default {
+  components: {
+    "seat-group": SeatGroup,
+    "seat-info-form": SeatInfoForm
+  },
+  props: {
+    seatsData: {
+      type: Object,
+      default() {
+        return {
+          tableA: [],
+          tableB: [],
+          tableC: [],
+          tableD: [],
+          tableE: [],
+          tableF: [],
+          tableG: [],
+          tableH: []
+        };
+      }
+    }
+  },
+  methods: {
+    openEditDialog(seatInfo) {
+      this.$refs["dialog"].handleOpen(seatInfo);
+    }
+  }
+};
 </script>
+
+<style>
+.odc-region {
+  margin: 20px;
+}
+</style>
